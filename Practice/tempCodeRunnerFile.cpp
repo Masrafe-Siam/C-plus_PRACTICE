eed9c18878de4850d13f97649931402a9ef40249
masrafe-siam <masrafesiammbhs1633@gmail.com>
2025-06-24 20:37:35 +0600

    Final update before archiving

---
+++ Practice/tempCodeRunnerFile.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+ 
+int main() {
+int a;
+cout<<"choose"<<endl;
+cin>>a;
+switch(a)
+{
+    case 1:
+    double take,amount;
+amount=4.00*take;
+cout<<"Total: R$ amount"<<endl;
+break;
+}
+    return 0;
+}
